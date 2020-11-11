be66dac0fe2102e510e412c4555df9332a74e228
trunghieu8668 <64822814+trunghieu8668@users.noreply.github.com>
2020-11-11 13:12:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ if(process.env.NODE_ENV == 'production'){
   app.use(express.static('client_nextjs/public'))
   const path = require('path')
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client_nextjs', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client_nextjs'))
   })
 }
 //Run
